cd134910f3126a9ede3cf52f70da1a79e765d566
Francois Kergaravat <francois.kergaravat@gmail.com>
2025-02-10 22:17:46 +0100

    rails crud

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,12 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
   get "tasks", to: "tasks#index", as: :tasks
+  get "tasks/new", to: "tasks#new", as: :new_task
   get "tasks/:id", to: "tasks#show", as: :task
+  post "tasks", to: "tasks#create", as: :create_task
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  patch "tasks/:id", to: "tasks#update", as: :task_update
+  delete "tasks/:id", to: "tasks#destroy", as: :task_destroy
   # Defines the root path route ("/")
   # root "posts#index"
 end
